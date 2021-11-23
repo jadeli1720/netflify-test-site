08cbb32c6c9d530945b9b49f74dbb0a103c26088
BillyBernard <billybernard54@gmail.com>
2021-11-22 21:34:20 -0700

    logo works on nav

--- my-portfolio-site/src/components/topNav/topNav.js
+++ my-portfolio-site/src/components/topNav/topNav.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './topNav.scss';
 
+
 export default function topNav() {
     return (
         <div className='topNav'>
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro">logo</a>
+                    <a href="#intro" className="logo"><img src="/images/logoTest.png" alt="William Paris Logo"/></a>
                 </div>
                 <div className="right">
 
