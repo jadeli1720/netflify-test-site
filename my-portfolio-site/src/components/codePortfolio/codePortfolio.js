3bc2831ac12fe4bf540714f2cc0108796eb7fa29
BillyBernard <billybernard54@gmail.com>
2021-11-24 09:50:09 -0700

    images displayed on portfolio site

--- my-portfolio-site/src/components/codePortfolio/codePortfolio.js
+++ my-portfolio-site/src/components/codePortfolio/codePortfolio.js
@@ -6,15 +6,22 @@ export default function codePortfolio() {
         <div className="codePortfolio" id="codePortfolio">
                 <h1>My Code</h1>
                 <ul>
-                    <li>Portfolio Site</li>
+                    <li className='active'>Portfolio Site</li>
                     <li>React Router Movies</li>
                     <li>Nasa Photo of The Day</li>
                 </ul>
                 <div className="container">
-                    <div className="item"></div>
-                    <img src="" alt=""/>
-                    <h3></h3>
-                </div>
+                    <div className="item">
+                    <img src="/images/PortfolioCapture.png" alt="Portfolio Page"/>
+                    <h3>My Portfolio Code</h3>
+                    <img src="/images/PlantCapture.png" alt="Plant Page"/>
+                    <h3>Plant Site Code</h3>
+                    <img src="/images/NasaCapture.png" alt="Nasa Page"/>
+                    <h3>Nasa Site Code</h3>
+                    <img src="/images/NasaCapture.png" alt="African Market"/>
+                    <h3>Current Project</h3>
+                    </div>
+                </div>    
         </div>
     )
 }
