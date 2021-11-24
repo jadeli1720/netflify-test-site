f5ad50aa498027430a39e2021a2ccc32f9da5d14
BillyBernard <billybernard54@gmail.com>
2021-11-24 10:10:36 -0700

    photos sized and titled

--- my-portfolio-site/src/components/codePortfolio/codePortfolio.js
+++ my-portfolio-site/src/components/codePortfolio/codePortfolio.js
@@ -9,16 +9,23 @@ export default function codePortfolio() {
                     <li className='active'>Portfolio Site</li>
                     <li>React Router Movies</li>
                     <li>Nasa Photo of The Day</li>
+                    <li>African Market</li>
                 </ul>
                 <div className="container">
                     <div className="item">
                     <img src="/images/PortfolioCapture.png" alt="Portfolio Page"/>
                     <h3>My Portfolio Code</h3>
+                    </div>
+                    <div className="item">
                     <img src="/images/PlantCapture.png" alt="Plant Page"/>
                     <h3>Plant Site Code</h3>
+                    </div>
+                    <div className="item">
                     <img src="/images/NasaCapture.png" alt="Nasa Page"/>
                     <h3>Nasa Site Code</h3>
-                    <img src="/images/NasaCapture.png" alt="African Market"/>
+                    </div>
+                    <div className="item">
+                    <img src="/images/CodeCapture.png" alt="African Market"/>
                     <h3>Current Project</h3>
                     </div>
                 </div>    
