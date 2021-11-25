a6bc47fe245e6c7e9b1995568d7e745be782819d
BillyBernard <billybernard54@gmail.com>
2021-11-24 18:04:36 -0700

    working on art portfolio

--- my-portfolio-site/src/components/artPortfolio/artPortfolio.js
+++ my-portfolio-site/src/components/artPortfolio/artPortfolio.js
@@ -4,7 +4,21 @@ import './artPortfolio.scss';
 export default function artPortfolio() {
     return (
         <div className="artPortfolio" id="artPortfolio">
-    
+            <h1>My Art</h1>
+            <div className="container">
+                <div className="item">
+                    <img src="/images/logoTest.png" alt="William Paris Logo"/>
+                    <h3>William Paris Logo</h3>
+                </div>
+                <div className="item">
+                    <img src="/images/FetchLogoFinal.png" alt="Fetch Logo"/>
+                    <h3>Fetch Logo</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="William Paris Logo"/>
+                    <h3>WIP</h3>
+                </div>
+            </div>
         </div>
     )
 }
--- my-portfolio-site/src/components/codePortfolio/codePortfolio.js
+++ my-portfolio-site/src/components/codePortfolio/codePortfolio.js
@@ -2,6 +2,22 @@ import React from 'react';
 import './codePortfolio.scss';
 
 export default function codePortfolio() {
+    const onClickGit = () => {
+        window.open(`https://github.com/BillyBernard`, "_blank")
+    }
+    const portfolioClick = () => {
+        window.open(`https://github.com/BillyBernard/myPortfolioSite`, "_blank")
+    }
+    const plantClick = () => {
+        window.open(`https://github.com/BillyBernard/web-sprint-challenge-advanced-react`, "_blank")
+    }
+    const nasaClick = () => {
+        window.open(`https://github.com/BillyBernard/nasa-photo-of-the-day`, "_blank")
+    }
+    const  marketClick = () => {
+        window.open(`https://github.com/new-african-market`, "_blank")
+    }
+    
     return (
         <div className="codePortfolio" id="codePortfolio">
                 <h1>My Code</h1>
@@ -14,23 +30,23 @@ export default function codePortfolio() {
                 </ul>
                 <div className="container">
                     <div className="item">
-                    <img src="/images/PortfolioCapture.png" alt="Portfolio Page"/>
+                    <img src="/images/PortfolioCapture.png" alt="Portfolio Page" onClick={portfolioClick}/>
                     <h3>My Portfolio Code</h3>
                     </div>
                     <div className="item">
-                    <img src="/images/PlantCapture.png" alt="Plant Page"/>
+                    <img src="/images/PlantCapture.png" alt="Plant Page" onClick={plantClick}/>
                     <h3>Plant Site Code</h3>
                     </div>
                     <div className="item">
-                    <img src="/images/NasaCapture.png" alt="Nasa Page"/>
+                    <img src="/images/NasaCapture.png" alt="Nasa Page" onClick={nasaClick}/>
                     <h3>Nasa Site Code</h3>
                     </div>
                     <div className="item">
-                    <img src="/images/CodeCapture.png" alt="African Market"/>
+                    <img src="/images/CodeCapture.png" alt="African Market" onClick={marketClick}/>
                     <h3>Current Project</h3>
                     </div>
                 </div>
-                <button className="git">My Github</button>
+                <button className="git" onClick={onClickGit}>My Github</button>
         </div>
     )
 }
