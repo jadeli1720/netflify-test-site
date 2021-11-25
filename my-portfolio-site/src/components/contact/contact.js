81ad9bba6cd00485a5ecf1fac3206c82999248ad
BillyBernard <billybernard54@gmail.com>
2021-11-25 16:25:49 -0700

    contact links added

--- my-portfolio-site/src/components/contact/contact.js
+++ my-portfolio-site/src/components/contact/contact.js
@@ -11,15 +11,19 @@ export default function Contact() {
     }
     return (
         <div className="contact" id="contact">
+            
             <div className="left">
                 <h1>Contact Me:</h1>
-                <h3>LinkedIn</h3>
-                <h3>GitHub</h3>
-                <h3>Email</h3>
+                <address>
+                <h3>LinkedIn: <a href="https://www.linkedin.com/in/william-paris-jr-692415154/" target="_blank" rel="noreferrer">https://www.linkedin.com/in/william-paris-jr-692415154/</a></h3>
+                <h3>GitHub: <a href="https://github.com/BillyBernard" target="_blank" rel="noreferrer">https://github.com/BillyBernard</a></h3>
+                <h3>Email: williamparis26@gmail.com</h3>
+                </address>
                 <img src="/images/Mail.png" alt="Mail"/>
+                <h4>Copyright &#169; William Paris</h4>
             </div>
             <div className="right">
-                <h2>Send me your thoughts on the site</h2>
+                <h2>Send me your thoughts</h2>
                     <form onSubmit={handleSubmit}>
                         <input type="text" placeholder="Type your email here"/>
                         <textarea placeholder="Type your message here"></textarea>
