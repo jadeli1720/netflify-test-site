29333ce73e277a0358f74a19e2e873b62924ae24
BillyBernard <billybernard54@gmail.com>
2021-11-25 15:54:15 -0700

    close to finished on contact page

--- my-portfolio-site/src/components/contact/contact.js
+++ my-portfolio-site/src/components/contact/contact.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
 import './contact.scss';
 
-export default function contact() {
+export default function Contact() {
+    const [mail, setMail] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMail(true);
+        console.log();
+    }
     return (
         <div className="contact" id="contact">
             <div className="left">
-                <h1>Find Me</h1>
+                <h1>Contact Me:</h1>
                 <h3>LinkedIn</h3>
                 <h3>GitHub</h3>
                 <h3>Email</h3>
                 <img src="/images/Mail.png" alt="Mail"/>
             </div>
             <div className="right">
-                <h2>Contact Me</h2>
-                    <form>
-                        <input type="text" placeholder="Email"/>
-                        <textarea placeholder="message"></textarea>
+                <h2>Send me your thoughts on the site</h2>
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" placeholder="Type your email here"/>
+                        <textarea placeholder="Type your message here"></textarea>
                         <button type="submit">Send</button>
+                        {mail && <span>Thank you for the message!</span>}
                     </form>
             </div>
         </div>
