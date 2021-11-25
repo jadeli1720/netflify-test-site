0b37522aecb4aa88ed56cae4c2d50d571b3e6f67
BillyBernard <billybernard54@gmail.com>
2021-11-25 15:27:31 -0700

    contact section layout and sizing

--- my-portfolio-site/src/components/contact/contact.js
+++ my-portfolio-site/src/components/contact/contact.js
@@ -4,7 +4,21 @@ import './contact.scss';
 export default function contact() {
     return (
         <div className="contact" id="contact">
-
+            <div className="left">
+                <h1>Find Me</h1>
+                <h3>LinkedIn</h3>
+                <h3>GitHub</h3>
+                <h3>Email</h3>
+                <img src="/images/Mail.png" alt="Mail"/>
+            </div>
+            <div className="right">
+                <h2>Contact Me</h2>
+                    <form>
+                        <input type="text" placeholder="Email"/>
+                        <textarea placeholder="message"></textarea>
+                        <button type="submit">Send</button>
+                    </form>
+            </div>
         </div>
     )
 }
